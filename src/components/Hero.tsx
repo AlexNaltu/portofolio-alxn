98b58e37d3072200c98e7dc2607ae25efe818dc0
AlexNaltu <naltuok@gmail.com>
2023-09-20 17:51:01 +0200

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,9 +8,8 @@ function Hero() {
       <div className="flex flex-col gap-2 items-center mb-10 text-center">
         <h1 className="text-xl sm:text-4xl lg:text-6xl font-bold">Successful Front-end Development</h1>
         <p className="text-xs sm:text-sm md:text-xl lg:text-3xl font-extralight">
-          Hi.I'm Alex, a Junior Front-end Developer with 6 months experience.
-          <br />
-          I'm 18 Years old , I finished 5 months training course at {""}
+          Hi I am Alex a Junior Front-end Developer with 6 months experience.
+          <br />I am 18 Years old. I finished 5 months training course at
           <span className="font-black text-purple-500 hover:text-purple-950 ease-in">
             <Link href="https://itschool.ro/">ITSCHOOL.</Link>
           </span>
--- src/components/PortofolioHero.tsx
+++ src/components/PortofolioHero.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
 
 function PortofolioHero() {
@@ -8,9 +7,9 @@ function PortofolioHero() {
       <div className="flex flex-col gap-2 items-center mb-10 text-center">
         <h1 className="text-xl sm:text-4xl lg:text-6xl font-bold">Successful Front-end Development</h1>
         <p className="text-xs sm:text-sm md:text-xl lg:text-3xl font-extralight">
-          Hi.I'm Alex, a Junior Front-end Developer with 6 months experience.
+          Hi.I am Alex, a Junior Front-end Developer with 6 months experience.
           <br />
-          I'm 18 Years old , I finished 5 months training course at {""}
+          I am 18 Years old , I finished 5 months training course at
           <span className="font-black text-purple-500 hover:text-purple-950 ease-in">
             <Link href="https://itschool.ro/">ITSCHOOL.</Link>
           </span>
